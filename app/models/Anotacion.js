aa3eab222340772c5e07a31a13be9a2c94612107
YuriiTonkov <alosadar@gmail.com>
2013-08-23 20:22:59 +0100

    prefs

--- app/models/Anotacion.js
+++ app/models/Anotacion.js
@@ -11,8 +11,13 @@ exports.definition = {
 		adapter: {
 			type: "sql",
             collection_name: "Anotacion",
+<<<<<<< HEAD
             db_file: "/AulaVirtual_v13.sqlite",
             db_name: "AulaVirtual",
+=======
+			db_file: "/AulaVirtual_v13.sqlite",            
+			db_name: "AulaVirtual",
+>>>>>>> 3c119669e2457d610aedeaf82207f28db26c2a05
             idAttribute: "IdAlumnoAsignatura",
             remoteBackup:"false"
 		}
