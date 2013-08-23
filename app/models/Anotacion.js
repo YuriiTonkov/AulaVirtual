64f9ba35deb675802e387c0c0c4da70560fac431
YuriiTonkov <alosadar@gmail.com>
2013-08-23 20:24:05 +0100

    correcion anotacion

--- app/models/Anotacion.js
+++ app/models/Anotacion.js
@@ -11,14 +11,10 @@ exports.definition = {
 		adapter: {
 			type: "sql",
             collection_name: "Anotacion",
-<<<<<<< HEAD
-            db_file: "/AulaVirtual_v13.sqlite",
-            db_name: "AulaVirtual",
-=======
+
 			db_file: "/AulaVirtual_v13.sqlite",            
 			db_name: "AulaVirtual",
->>>>>>> 3c119669e2457d610aedeaf82207f28db26c2a05
-            idAttribute: "IdAlumnoAsignatura",
+			idAttribute: "IdAlumnoAsignatura",
             remoteBackup:"false"
 		}
 	},		
